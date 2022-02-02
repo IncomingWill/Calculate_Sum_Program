92c736540f2cbc560f951bdc80fcab91b3e5ab78
IncomingWill <thewilliamedwards@gmail.com>
2022-02-01 19:48:57 -0600

    Add project files.

---
+++ PythonApplication1/calc_sum.py
@@ -0,0 +1,7 @@
+# Title: Calculate Sum Program
+# Program created by William Schaeffer
+# CPS 313
+# P. 358, Exercise 5, Calculate Sum Program
+# 02.01.22
+
+# This program will calculate the sum of numbers stored in numbers.txt
